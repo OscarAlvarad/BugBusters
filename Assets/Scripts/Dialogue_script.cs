c6a0d752481039e218067d5039622c821ca64ee4
MGRILLO - Fletcher <96928501+Fletcher17b@users.noreply.github.com>
2023-09-19 20:31:15 -0600

    :p

---
+++ Assets/Scripts/Dialogue_script.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Linq;
+
+public class Dialogue_script : MonoBehaviour
+{
+    [SerializeField] public GameObject Text_panel;
+    [SerializeField] public TextMeshProUGUI Tutorial_text;
+    public bool finished = false;
+
+    public string[] lines;
+    public bool islinesempy = false;
+    public int lengthofline = 0;
+    public float textspeed = 0.1f;
+    int index = 0;
+
+    void Start()
+    {
+        Start_Text();
+    }
+
+    public void Start_Text()
+    {
+        index = 0;
+        StartCoroutine(Writeline());
+    }
+
+    IEnumerator Writeline()
+    {
+        lengthofline = lines.Length;
+        if (lengthofline != 0)
+        {
+            foreach (char item in lines[index].ToCharArray())
+            {
+                Tutorial_text.text += item;
+
+                yield return new WaitForSeconds(textspeed);
+            }
+        } else
+        {
+            Debug.Log("it went bananas");
+        }
+        
+
+        finished = true;
+    }
+
+
+
+}
--- Assets/panel_scrip1.cs
+++ Assets/panel_scrip1.cs
@@ -4,36 +4,44 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class panel_scrip1 : MonoBehaviour
 {
 
-
-    [SerializeField]
-
-    public GameObject target_panel;
+   // para hide_panel
+    [SerializeField] public GameObject target_panel;
+    public Button button;
     public static int counter;
-    
+    bool text_finished = false;
+    Dialogue_script dialogue_Script;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        target_panel.gameObject.SetActive(true);
-        if (counter >= 1 )
-        {
-            target_panel.gameObject.SetActive(false);
-        }
-        
+        Dialogue_script dialogue_Script = GetComponent<Dialogue_script>();
+        bool text_finished = dialogue_Script.finished;
+
+        button.interactable = false;
+
+            target_panel.gameObject.SetActive(true);
+            if (counter >= 1)
+            {
+                target_panel.gameObject.SetActive(false);
+            }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void update()
     {
-        
+        bool text_finished = dialogue_Script.finished;
+        if (text_finished)
+        {
+            button.interactable = true; 
+        }
+        else
+        {
+            button.interactable = false;
+        }
     }
-
-
 
     public void hide_panel()
     {
@@ -41,7 +49,7 @@ public class panel_scrip1 : MonoBehaviour
         target_panel.gameObject.SetActive(false);
     }
 
-
+    
     
 
 }
