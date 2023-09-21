1595785a2880138be511fa52f08864f25f8bf368
OscarAlvarad <oscar.jironalvarado@gmail.com>
2023-09-21 16:26:03 -0600

    Portal Wall Lvl1

--- Assets/Scripts/WordCollector.cs
+++ Assets/Scripts/WordCollector.cs
@@ -10,6 +10,8 @@ public class WordCollector : MonoBehaviour
         { 
             GetComponent<SpriteRenderer>().enabled = false;
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
             FindAnyObjectByType<WordManager>().AllWordCollected(); 
             Destroy(gameObject, 0.5f);
         }
