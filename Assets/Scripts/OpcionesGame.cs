edb829e1f0eb07b4cffca8058183577f7cd83c5b
OscarAlvarad <oscar.jironalvarado@gmail.com>
2023-09-23 10:38:05 -0600

    Panel Options in Game

---
+++ Assets/Scripts/OpcionesGame.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpcionesGame : MonoBehaviour
+{
+    public GameObject optionsPanel;
+
+    public void OptionsPanel()
+    {
+        Time.timeScale = 0;
+        optionsPanel.SetActive(true);
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1;
+        optionsPanel.SetActive(false);
+    }
+
+    public void Options()
+    {
+        //Panel Opciones
+    }
+
+    public void MenuNiveles()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Nivel 1");
+    }
+
+}
