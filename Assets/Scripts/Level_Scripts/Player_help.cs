6dcc11d380cd920235b06934b2f119b58c688bdd
MGRILLO - Fletcher <96928501+Fletcher17b@users.noreply.github.com>
2023-10-02 19:16:51 -0600

    refresh
    refresh

--- Assets/Scripts/Level_Scripts/Player_help.cs
+++ Assets/Scripts/Level_Scripts/Player_help.cs
@@ -13,6 +13,7 @@ public class Player_help : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI Tutorial_text;
     [SerializeField] public Button help_ok_button;
+    [SerializeField] public GameObject Panel;
 
     public GameObject Player;
 
@@ -21,14 +22,23 @@ public class Player_help : MonoBehaviour
     public bool frozen = false;
     public bool text_started = false;
     public bool button_pressed_flag = false;
+    public bool WriteLineBreak = false;
+
+
+    public int PanelCounter = 0;
 
     public string[] lines;
     public int lengthofline = 0;
     public float textspeed = 0.1f;
 
+    int DialogueIndex = 0;
+    int OkButtonTracker = 0;
+    
+
 
     void Start()
     {
+        Panel.SetActive(false);
         help_ok_button.gameObject.SetActive(false);
         Player = GameObject.Find("Carlos");
     }
@@ -39,16 +49,19 @@ public class Player_help : MonoBehaviour
   
         float position_x = Player.transform.position.x;
         player_in_position = IsPlayerInPosition(position_x);
-
+              
         if (player_in_position)
         {
-            frozen = true;           
+            frozen = true;
+            if (PanelCounter < 1) { Panel.SetActive(true); }
+            PanelCounter++;
+                       
             if (!text_started)
             {
                 StartText();
                 text_started = true;
             }
-
+                       
             if (finished) {
                
                 if (!button_pressed_flag) 
@@ -60,11 +73,17 @@ public class Player_help : MonoBehaviour
                 frozen = false;
             }
 
-        } else
-        {
-            frozen = false;
-        }
+        } else {frozen = false;}
 
+    }
+
+    public void PanelShow()
+    {
+        Tutorial_text.SetText("");
+        Panel.SetActive(true);
+        Tutorial_text.gameObject.SetActive(true);
+        help_ok_button.gameObject.SetActive(true);
+        StartText();
     }
 
     private bool IsPlayerInPosition(float PositionInX)
@@ -74,25 +93,50 @@ public class Player_help : MonoBehaviour
 
     public void HelpOkButtonFunction()
     {
-        Tutorial_text.gameObject.SetActive(false);
-        help_ok_button.gameObject.SetActive(false);
+
+        if (text_started)
+        {
+            switch (OkButtonTracker)
+            {
+                case 0:
+                    WriteNextLine();                    
+                    break;    
+                case 1:
+                    WriteNextLine();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        OkButtonTracker++;
+
+        if (OkButtonTracker >= 3)
+        {
+            Panel.SetActive(false);
+            Tutorial_text.gameObject.SetActive(false);
+            help_ok_button.gameObject.SetActive(false);
+            OkButtonTracker = 0;
+        }
+        
     }
 
     public void StartText()
     {
-       
+        DialogueIndex= 0;
         StartCoroutine(Writeline());
     }
 
     IEnumerator Writeline()
     {
+        finished = false;
         lengthofline = lines.Length;
         if (lengthofline != 0)
         {
-            foreach (char item in lines[0].ToCharArray())
+            foreach (char item in lines[DialogueIndex].ToCharArray())
             {
                 Tutorial_text.text += item;
-
+                if (WriteLineBreak) { Tutorial_text.SetText(""); break; }
                 yield return new WaitForSeconds(textspeed);
             }
         }
@@ -102,6 +146,21 @@ public class Player_help : MonoBehaviour
         }
 
 
+        finished = true;
+    }
+
+    public void WriteNextLine()
+    {
+        finished = false;
+        if (DialogueIndex < lengthofline-1)
+        {
+            DialogueIndex++;
+            Tutorial_text.SetText("");
+            StartCoroutine(Writeline());
+        } else
+        {
+            Debug.Log("Lmao");
+        }
         finished = true;
     }
 
